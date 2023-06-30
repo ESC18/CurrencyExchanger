d567d08a74829710914156d56fc1bd320a74efb6
ESC18 <ponderosaprintingus@gmail.com>
2023-06-30 10:56:09 -0700

    Add Index.JS file with initial planning and import for currenc.js in place

--- src/index.js
+++ src/index.js
@@ -2,4 +2,49 @@
 import './assets/images/SC.png';
 import './assets/fonts.css';
 import './css/styles.css';
+import Currency from './currency.js'
 //EX: import Blank from '/.js/secondary.js';
+
+//Business Logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//UI Logic
