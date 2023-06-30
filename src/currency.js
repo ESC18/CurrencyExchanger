455901f7310c9ac28143ba44743453d7d6fb1c40
ESC18 <ponderosaprintingus@gmail.com>
2023-06-30 10:48:53 -0700

    Add currency.js and fill it with the proper class and functions needed to initially get the API running.

--- src/currency.js
+++ src/currency.js
@@ -1,3 +1,17 @@
 export default class Currency {
-
+    static currencyExecutor(amount) {
+        const apiKey = process.env.API_KEY;
+        return fetch(
+            `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`
+        ).then(function (response) {
+            if (response.ok != true) {
+                const errorMessage = `${response.status} ${response.statusText}`
+                throw new Error(errorMessage);
+            } else {
+                return response.json();
+            }
+        }).catch(function (error) {
+            return "There is an error:" + error;
+        });
+    }
 }
