bfc9d15f72702d71038a0b2c44b581782a67f08e
ESC18 <ponderosaprintingus@gmail.com>
2023-06-30 17:35:31 -0700

    Complete Logic for both UI and Business. Seperates UI logic from business Logic. Adds additional error explaining functionality.

--- src/index.js
+++ src/index.js
@@ -3,21 +3,30 @@ import './assets/fonts.css';
 import './css/styles.css';
 import Currency from './currency';
 
-
+// Business Logic
 function currencyExecutor(baseCurrency, amount, exchangeCurrency) {
-    Currency.currencyExecutor(baseCurrency)
+    return Currency.currencyExecutor(baseCurrency)
         .then(function (response) {
             const conversionRates = response.conversion_rates;
             if (conversionRates) {
                 const conversionRate = conversionRates[exchangeCurrency];
                 if (!conversionRate) {
-                    console.log(`Conversion rate for ${exchangeCurrency} not found.`);
-                    return;
+                    throw new Error(`Conversion rate for "${exchangeCurrency}" not found.`);
                 }
+                return amount * conversionRate;
             } else {
-                console.log(response);
+                throw new Error('Response does not contain conversion rates.');
             }
+        })
+        .catch(function (error) {
+            displayError(`There is an error: ${error}`);
         });
+}
+
+// UI Logic
+function displayError(message) {
+    const resultElement = document.getElementById("result");
+    resultElement.innerText = message;
 }
 
 function submission(event) {
@@ -26,14 +35,18 @@ function submission(event) {
     const amount = parseFloat(document.getElementById("amount").value);
     const exchangeCurrency = document.getElementById("exchangeCurrency").value;
 
-    currencyExecutor(baseCurrency, amount, exchangeCurrency);
+    currencyExecutor(baseCurrency, amount, exchangeCurrency)
+        .then(function (convertedAmount) {
+            if (convertedAmount !== undefined) {
+                const resultElement = document.getElementById("result");
+                resultElement.innerText = `Converted amount: ${convertedAmount} ${exchangeCurrency}`;
+            }
+        });
 }
 
 window.addEventListener("load", function () {
     document.querySelector("form").addEventListener("submit", submission);
 });
-
-
 
 
 
